89e9512cd6ede2ff1d7490c49872e46115cca10e
Chidera0001 <c.anele@alustudent.com>
2023-12-12 20:03:06 +0200

    new changes

--- main.js
+++ main.js
@@ -1,61 +1,78 @@
-document.addEventListener('DOMContentLoaded', loadAPIs);
+document.addEventListener("DOMContentLoaded", function () {
+  const randomImageURL = getRandomImage();
+  document.body.style.backgroundImage = `url(${randomImageURL})`;
+});
 
-function loadAPIs (){
-    button2function();
-    joke();
-    convert();
+document.addEventListener("DOMContentLoaded", function () {
+  const randomImageURL = getRandomImage();
+  document.body.style.backgroundImage = `url(${randomImageURL})`;
+});
+document.addEventListener("DOMContentLoaded", loadAPIs);
+
+function loadAPIs() {
+  button2function();
+  joke();
+  convert();
 }
 
-function button2function (){
-    document.getElementById('buttonGetJoke').onclick = joke;
-    document.getElementById('buttonCurrency').onclick = convert;
+function button2function() {
+  document.getElementById("buttonGetJoke").onclick = joke;
+  document.getElementById("buttonCurrency").onclick = convert;
 }
 
 async function joke() {
-    await fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', {
-	    method: 'GET',
-	    headers: {
-		    'X-RapidAPI-Key': '9220814e8bmshc6822d31636dc7bp1cf24ajsn7206fa4eb740',
-		    'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
-            'accept': 'application/json'
-	    }
+  await fetch(
+    "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "9220814e8bmshc6822d31636dc7bp1cf24ajsn7206fa4eb740",
+        "X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+        accept: "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log("Chuck Norris API object:");
+      console.log(response);
+      console.log("\n");
+      document.getElementById("joke").innerText = response.value;
+      document.getElementsByClassName("jokeTitle")[0].href = response.url;
     })
 
-    .then(response => response.json())
-    .then(response => {
-        console.log("Chuck Norris API object:");
-        console.log(response);
-        console.log("\n");
-        document.getElementById('joke').innerText = response.value
-        document.getElementById('jokeIcon').src = response.icon_url;
-        document.getElementsByClassName('jokeTitle')[0].href = response.url;
-    })
-    
-    .catch (error => {
-	console.log(error);
+    .catch((error) => {
+      console.log(error);
     });
 }
 
 async function convert() {
-    const from = document.getElementById('inputCurrencyFrom').value;
-    const to = document.getElementById('inputCurrencyTo').value;
-    await fetch('https://currency-exchange.p.rapidapi.com/exchange?from='+ from +'&to='+ to + '&q=1.0', {
-	    method: 'GET',
-	    headers: {
-		'X-RapidAPI-Key': '9220814e8bmshc6822d31636dc7bp1cf24ajsn7206fa4eb740',
-		'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-	    }
+  const from = document.getElementById("inputCurrencyFrom").value;
+  const to = document.getElementById("inputCurrencyTo").value;
+  await fetch(
+    "https://currency-exchange.p.rapidapi.com/exchange?from=" +
+      from +
+      "&to=" +
+      to +
+      "&q=1.0",
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "9220814e8bmshc6822d31636dc7bp1cf24ajsn7206fa4eb740",
+        "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log("Currency Exchange API object:");
+      console.log(response);
+      console.log("\n");
+      document.getElementById("currencyResult").innerHTML =
+        "Result: " + response;
     })
 
-    .then(response => response.json())
-    .then(response => {
-        console.log("Currency Exchange API object:");
-        console.log(response);
-        console.log("\n");
-        document.getElementById('currencyResult').innerHTML = 'Result: ' + response;
-    })
-
-    .catch (error => {
-	    console.error(error);
+    .catch((error) => {
+      console.error(error);
     });
 }
